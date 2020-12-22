f473b0b3e2820c79061ba323e7a15ac730e65e87
huanli9 <huanli9@iflytek.com>
2020-12-22 15:19:51 +0800

    20201221

---
+++ mvc-annotation/src/main/java/com/jason/lee/mvc/controller/HelloController.java
@@ -0,0 +1,31 @@
+package com.jason.lee.mvc.controller;
+
+
+import com.jason.lee.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author huanli9
+ * @description
+ * @date 2020/11/30 10:26
+ */
+@Controller
+public class HelloController {
+
+    @Autowired
+    HelloService service;
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return service.sayHello("Tomcat");
+    }
+
+    @RequestMapping("/success")
+    public String success() {
+        return "success";
+    }
+}
